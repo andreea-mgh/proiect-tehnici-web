d35b28dd086b2ddc70f4ee2e15915e8609a7c61a
andreea-mgh <andreea.meghe@gmail.com>
2025-06-27 22:40:57 +0300

    erori pagini

--- index.js
+++ index.js
@@ -15,6 +15,28 @@ app.get(['/', '/index', '/home'], (req, res) => {
     res.render('pagini/index', { title: 'Proiect Tehnici Web' });
 });
 
+app.get("/:pagina", (req, res) => {
+    // const pagina = req.params[0];
+    const pagina = req.params.pagina || 'index';
+    console.log(`Cerere pentru pagina: ${pagina}`);
+
+    res.render(pagina, (err, html) => {
+        if (err) {
+            console.error(`Eroare la încărcarea paginii "${pagina}":`, err.message);
+            
+            if (err.message.startsWith("Failed to lookup view")) {
+                return res.status(404).render('pagini/404', { pagina });
+            } else {
+                return res.status(500).render('pagini/eroare', { eroare: err });
+            }
+        }
+        res.send(html);
+    });
+});
+
+
+
+
 
 
 
