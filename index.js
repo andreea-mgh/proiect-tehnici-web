4fa40f5cba78d890092021c2ad7351284c0a68c8
andreea-mgh <andreea.meghe@gmail.com>
2025-06-28 08:58:09 +0300

    compilare automata scss

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
+const sass = require('sass');
 
 
 const app = express();
 
 // generat foldere
-const vect_foldere = ["temp"];
+const vect_foldere = ["temp", "backup"];
 
 vect_foldere.forEach(folder => {
     const caleFolder = path.join(__dirname, folder);
@@ -21,7 +22,9 @@ vect_foldere.forEach(folder => {
 // erori (global???)
 
 global.obGlobal = {
-    obErori: null
+    obErori: null,
+    folderScss: path.join(__dirname, 'resurse', 'scss'),
+    folderCss: path.join(__dirname, 'resurse', 'css'),
 };
 function initErori() {
     const eroriJson = JSON.parse(fs.readFileSync('erori.json', 'utf8'));
@@ -49,6 +52,58 @@ function afiseazaEroare(res, identificator) {
     imagine: eroare.imagine
   });
 }
+
+
+// SCSS
+function compileazaScss(caleScss, caleCss) {
+    if (!path.isAbsolute(caleScss)) {
+        caleScss = path.join(obGlobal.folderScss, caleScss);
+    }
+
+    if (!caleCss) {
+        const numeFisier = path.basename(caleScss, ".scss") + ".css";
+        caleCss = path.join(obGlobal.folderCss, numeFisier);
+    } else if (!path.isAbsolute(caleCss)) {
+        caleCss = path.join(obGlobal.folderCss, caleCss);
+    }
+
+    // compilare
+    try {
+        const rezultat = sass.compile(caleScss, { style: "expanded" });
+        // backup
+        const now = new Date();
+        const timestamp = now.toISOString().replace(/[-:.TZ]/g, "_");
+        const caleBackup = path.join(__dirname, 'backup', `${path.basename(caleCss, '.css')}_${timestamp}.css`);
+        if (fs.existsSync(caleCss)) {
+            fs.copyFileSync(caleCss, caleBackup);
+            console.log(`Backup creat: ${caleBackup}`);
+        }
+        fs.writeFileSync(caleCss, rezultat.css);
+        console.log(`Compilat: ${caleScss} -> ${caleCss}`);
+    } catch (err) {
+        console.error("Eroare la compilarea SCSS:", err);
+    }
+}
+
+// compileaza la pornire
+const fisiereScss = fs.readdirSync(obGlobal.folderScss).filter(fisier => fisier.endsWith('.scss'));
+fisiereScss.forEach(fisier => {
+    const caleScss = path.join(obGlobal.folderScss, fisier);
+    compileazaScss(caleScss);
+});
+
+// compileaza pe parcurs
+fs.watch(obGlobal.folderScss, (eventType, filename) => {
+    if (filename && filename.endsWith(".scss")) {
+        const caleScss = path.join(obGlobal.folderScss, filename);
+        console.log(`Fișierul ${filename} a fost modificat. Recompilare...`);
+        if (fs.existsSync(caleScss)) {
+            compileazaScss(caleScss);
+        }
+    }
+});
+
+
 
 // galerie
 function oraInInterval(oraCurenta, interval) {
